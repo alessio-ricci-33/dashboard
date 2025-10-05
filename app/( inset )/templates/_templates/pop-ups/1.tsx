f3a57b2c66ac994c1453f973bb807b9f50ced5fc
Patrick <simonettipatrick88@gmail.com>
2025-10-05 15:02:11 +0200

    .

--- app/( inset )/templates/_templates/pop-ups/1.tsx
+++ app/( inset )/templates/_templates/pop-ups/1.tsx
@@ -1,38 +1,43 @@
-import { Layer, Rect, Text } from 'react-konva';
-import { LocalImage } from '@/utils/local-image';
-import { useState } from 'react';
-import { Dialog, DialogContent, DialogTrigger } from '@/ui/dialog';
-import { Button } from '@/ui/button';
-import { Input } from '@/ui/input';
-import { Separator } from '@/ui/separator';
-import DownloadableCanvas from '@/components/downloadable-canvas';
-import { ParamsToProps } from '@/types/utils';
+import { Circle, Group, Layer, Rect, Text } from "react-konva";
+import { LocalImage, SvgIconImage } from "@/utils/local-image";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "@/ui/dialog";
+import { Button } from "@/ui/button";
+import { Input } from "@/ui/input";
+import { Separator } from "@/ui/separator";
+import DownloadableCanvas from "@/components/downloadable-canvas";
+import { ParamsToProps } from "@/types/utils";
+
+import { BsBoxArrowInUpRight } from "react-icons/bs";
+import { ImArrowUpRight2 } from "react-icons/im";
+
+import { params as IslandParams } from "./2";
 
 export const params = {
 	height: {
 		type: Number,
-		label: 'Altezza',
-		default: 100,
+		label: "Altezza",
+		default: 120,
 	},
 	width: {
 		type: Number,
-		label: 'Larghezza',
+		label: "Larghezza",
 		default: 350,
 	},
 	brandName: {
 		type: String,
-		label: 'Nome del brand',
-		default: 'messaggi.italia',
+		label: "Nome del brand",
+		default: "messaggi.italia",
 	},
 	message: {
 		type: String,
-		label: 'Messaggio del popup',
-		default: 'Testo desiderato...',
+		label: "Messaggio del popup",
+		default: "Testo desiderato...",
 	},
 	cta: {
 		type: String,
-		label: 'CTA',
-		default: 'Vai su 📲msgi.it',
+		label: "CTA",
+		default: "Vai su 📲msgi.it",
 	},
 } as const;
 
@@ -41,11 +46,10 @@ export const defaultProps: Props = Object.entries(params).reduce(
 	{}
 );
 
-const PADDING = 20;
+const PADDING = 14,
+	MARGIN = 21;
 
-export const Canvas = (
-	props = defaultProps as Props & { download?: boolean; onDownload?: () => void }
-) => {
+export const Canvas = (props = defaultProps as Props & { download?: boolean; onDownload?: () => void }) => {
 	const localProps = Object.entries(params).reduce((acc, [key, { default: defaultValue }]) => {
 		if (props[key]) acc[key] = props[key];
 		else acc[key] = defaultValue;
@@ -57,70 +61,107 @@ export const Canvas = (
 
 	return (
 		<DownloadableCanvas
+			fileType="png"
+			filename="expanded-popup"
 			download={props.download}
 			onDownload={props.onDownload}
-			width={width + PADDING * 2}
-			height={height + PADDING * 2}
+			width={width + MARGIN * 2}
+			height={height + MARGIN * 2}
 			options={{ preserveDrawingBuffer: true }}
-			style={{ backgroundColor: 'transparent', overflow: 'visible' }}>
+			style={{ backgroundColor: "transparent", overflow: "visible" }}
+		>
 			<Layer>
 				<Rect
-					x={PADDING}
-					y={PADDING}
+					x={MARGIN}
+					y={MARGIN}
 					width={width}
 					height={height}
 					fillLinearGradientStartPoint={{ x: 0, y: 0 }}
 					fillLinearGradientEndPoint={{ x: 0, y: height }}
-					fillLinearGradientColorStops={[0, '#e7efff', 1, '#56515a']}
+					fillLinearGradientColorStops={[0, "#2f2f32", 0.85, "#000000"]}
 					shadowBlur={10}
-					cornerRadius={10}
-					shadowColor="#000000"
-					stroke="#e7efff" // colore del bordo
+					cornerRadius={30}
+					shadowColor="#ffffff"
+					shadowOpacity={0.5}
+					stroke="#2f2f32" // colore del bordo
 					strokeWidth={0.5}
 				/>
+				<Group
+					x={MARGIN + PADDING}
+					y={MARGIN + PADDING}
+					width={width - MARGIN - PADDING}
+					height={height - MARGIN - PADDING}
+				>
+					<Group x={0} y={-3}>
+						{/* Immagine locale sopra il rettangolo */}
+						<LocalImage
+							src="/messaggi-italia.png" // immagine salvata in public/images/logo.png
+							height={IslandParams.height.default - PADDING}
+							shadowColor="#15171c"
+							shadowBlur={7}
+						/>
 
-				{/* Immagine locale sopra il rettangolo */}
-				<LocalImage
-					src="/messaggi-italia.png" // immagine salvata in public/images/logo.png
-					x={PADDING + 10}
-					y={PADDING + 10}
-					height={27}
-					shadowColor="#000000"
-					shadowBlur={7}
-				/>
+						<Text
+							x={IslandParams.height.default - PADDING + 8}
+							y={PADDING / 2}
+							text={brandName}
+							lineHeight={1.1}
+							fontSize={21}
+							fontFamily="logo" // deve corrispondere al nome definito in @font-face
+							fill="white"
+							opacity={0.75}
+						/>
+					</Group>
 
-				<Text
-					x={PADDING + 10 + 30}
-					y={PADDING + 11}
-					text={brandName}
-					fontSize={20}
-					fontFamily="logo" // deve corrispondere al nome definito in @font-face
-					fill="black"
-				/>
+					<Group y={height - IslandParams.height.default - PADDING} x={3}>
+						<Text
+							text={cta}
+							fontSize={16}
+							lineHeight={1}
+							fontFamily="secondary" // deve corrispondere al nome definito in @font-face
+							fill="#ffffff"
+							shadowColor="#ffffff"
+							shadowBlur={4}
+							shadowOffsetY={3}
+							shadowOpacity={0.25}
+							opacity={0.85}
+						/>
 
-				<Text
-					x={PADDING + 10}
-					y={height - 10 - PADDING}
-					text={message}
-					fontSize={18}
-					fontFamily="secondary" // deve corrispondere al nome definito in @font-face
-					fill="#ffffff"
-					shadowColor="#000000"
-					shadowBlur={4}
-					shadowOffsetY={3}
-				/>
+						<Text
+							y={PADDING / 2 + 18}
+							text={message}
+							lineHeight={1}
+							fontSize={18}
+							fontFamily="secondary" // deve corrispondere al nome definito in @font-face
+							fill="#ffffff"
+							shadowColor="#ffffff"
+							shadowBlur={4}
+							shadowOffsetY={3}
+							shadowOpacity={0.25}
+						/>
+					</Group>
 
-				<Text
-					x={PADDING + 10}
-					y={height - 10}
-					text={cta}
-					fontSize={16}
-					fontFamily="secondary" // deve corrispondere al nome definito in @font-face
-					fill="#ffffff"
-					shadowColor="#000000"
-					shadowBlur={4}
-					shadowOffsetY={3}
-				/>
+					<Group
+						x={width - PADDING * 2 - (IslandParams.height.default - PADDING - 4) / 2}
+						y={(IslandParams.height.default - PADDING - 4) / 2 - 1}
+					>
+						<Circle
+							radius={999}
+							fill="#15171c"
+							shadowBlur={7}
+							shadowColor="#ffffff"
+							shadowOpacity={0.5}
+							height={IslandParams.height.default - PADDING - 4}
+							width={IslandParams.height.default - PADDING - 4}
+						/>
+						<SvgIconImage
+							Icon={<ImArrowUpRight2 size={18} color="white" opacity={0.75} />}
+							height={18}
+							x={-(IslandParams.height.default - PADDING - 4) / 2 + 10}
+							y={-(IslandParams.height.default - PADDING - 4) / 2 + 10}
+						/>
+					</Group>
+				</Group>
 			</Layer>
 		</DownloadableCanvas>
 	);
@@ -133,65 +174,53 @@ export const component = (oldProps: Props = defaultProps) => {
 
 	return (
 		<Dialog
-			onOpenChange={open => {
+			onOpenChange={(open) => {
 				setDialogOpen(open);
 				if (!open) setToDownload(false);
-			}}>
-			<DialogTrigger key={'dialog-trigger'}>
+			}}
+		>
+			<DialogTrigger key={"dialog-trigger"}>
 				<Canvas download={false} {...oldProps} />
 			</DialogTrigger>
 			<DialogContent
-				key={'dialog-content'}
-				className="flex flex-row items-center gap-p p-p border border-zinc-600/50 !max-w-none w-fit shadow-[0_0_7px_-1px_#b0e9ff4a]">
+				key={"dialog-content"}
+				className="flex flex-row items-center gap-p p-p border border-zinc-600/50 !max-w-none w-fit shadow-[0_0_7px_-1px_#b0e9ff4a]"
+			>
 				<div className="contents">
-					<Canvas
-						download={toDownload}
-						onDownload={() => setToDownload(false)}
-						{...props}
-					/>
+					<Canvas download={toDownload} onDownload={() => setToDownload(false)} {...props} />
 				</div>
 				<Separator
 					orientation="vertical"
 					className="![background-color:transparent] bg-radial-[at_center] from-white to-transparent !h-[-webkit-fill-available]"
 				/>
 				<div className="flex flex-col gap-sidebar-p w-full">
-					{Object.entries(params).map(
-						([key, { label, default: defaultValue }], index) => (
-							<span key={index + key}>
-								{label}
-								:
-								<Input
-									type={
-										typeof defaultValue === 'number'
-											? 'number'
-											: 'text'
-									}
-									className="min-w-60"
-									onChange={e => {
-										if (!e.target.value?.trim().length)
-											return;
-										setProps({
-											...props,
-											[key]:
-												typeof defaultValue ===
-												'number'
-													? parseInt(
-															e.target
-																.value
-													  )
-													: e.target.value,
-										});
-									}}
-									defaultValue={props[label] ?? defaultValue}
-								/>
-							</span>
-						)
-					)}
+					{Object.entries(params).map(([key, { label, default: defaultValue }], index) => (
+						<span key={index + key}>
+							{label}
+							:
+							<Input
+								type={typeof defaultValue === "number" ? "number" : "text"}
+								className="min-w-60"
+								onChange={(e) => {
+									if (!e.target.value?.trim().length) return;
+									setProps({
+										...props,
+										[key]:
+											typeof defaultValue === "number"
+												? parseInt(e.target.value)
+												: e.target.value,
+									});
+								}}
+								defaultValue={props[label] ?? defaultValue}
+							/>
+						</span>
+					))}
 					<Button
 						className="mt-p cursor-pointer"
 						variant="outline"
 						size="sm"
-						onClick={() => setToDownload(true)}>
+						onClick={() => setToDownload(true)}
+					>
 						Download
 					</Button>
 				</div>
