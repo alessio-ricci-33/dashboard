50f8b0ea55d8b1525dc7de2b1e9802cfd092798d
PatrickDev-it <github@patrickdev.it>
2025-10-16 04:44:29 +0200

    .

--- app/api/history/shorts/[id]/route.ts
+++ app/api/history/shorts/[id]/route.ts
@@ -7,7 +7,7 @@ export const GET = async (req: Request, { params }: { params: { id: string } }) 
 		const { id: video_id } = await params;
 
 		const short = await Short.findOne({ video_id }).lean();
-		if (!short) return NextResponse.json({ error: 'Video not found' }, { status: 404 });
+		if (!short) throw new Error('Short not found.');
 
 		const diffs = short.views.splice(0, 1);
 		for (const views of short.views) {
--- utils/_try.ts
+++ utils/_try.ts
@@ -1,14 +1,16 @@
+import { NextResponse } from 'next/server';
+
 export default async function _try<R extends () => any>(
 	action: R,
 	on = {
-		success: (data: Awaited<ReturnType<R>>): any => Response.json({ success: 1, data }),
+		success: (data: Awaited<ReturnType<R>>): any => NextResponse.json({ success: 1, data }),
 		error: ({ cause, message, stack }: Error) => {
 			console.error(
 				`| Cause => ${cause}\n| Error message in MIDDLE handlers => ${message}\n`,
 				`\n| Stack => ${stack}`
 			);
 
-			return Response.json({ success: 0, message });
+			return NextResponse.json({ success: 0, message });
 		},
 	}
 ) {
