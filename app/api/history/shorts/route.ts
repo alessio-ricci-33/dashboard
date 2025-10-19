3c37f18a03cb33685f4c0f9078cf66c8e0557d75
PatrickDev-it <github@patrickdev.it>
2025-10-19 19:47:38 +0200

    .

--- app/api/history/shorts/route.ts
+++ app/api/history/shorts/route.ts
@@ -1,6 +1,9 @@
 import { Short } from '@/models/analytics';
 import _try from '@/utils/_try';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export const GET = async (req: Request) =>
 	await _try(async () => {
 		const ids = await Short.find({}).lean();
--- app/layout.tsx
+++ app/layout.tsx
@@ -43,9 +43,6 @@ export const viewport: Viewport = {
 	themeColor: '#000',
 };
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-
 export default function RootLayout({ sidebar, children: inset }: Layouts) {
 	return (
 		<html
