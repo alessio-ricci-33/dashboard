e339e3cca4a2cb0a5a2d57fa7c9985b99896522b
PatrickDev-it <github@patrickdev.it>
2025-11-11 13:41:09 +0100

    .

--- types/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-	var socket: WebSocket & {
-		emit: (event: string, data: any) => void;
-		emitBuffer: (data: any) => void;
-		on: (event: string, callback: (data: any) => void) => void;
-	};
-}
-
-export {};
